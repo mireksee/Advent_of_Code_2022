3e724b2ef9824b8a70005028a2038ea2f16be5cd
Mirosław Seehawer <mireksee@gmail.com>
2022-12-07 09:57:34 +0100

    4 day

---
+++ 4/main4_2.py
@@ -0,0 +1,19 @@
+input = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+lists = input.strip().split("\n")
+
+count = 0
+for pair in lists:
+    elf1, elf2 = pair.split(",")
+    elf1_nr1, elf1_nr2 = elf1.split("-")
+    elf2_nr1, elf2_nr2 = elf2.split("-")
+
+    if int(elf2_nr1) <= int(elf1_nr2) and int(elf1_nr1) <= int(elf2_nr2):
+        count +=1
+
+print(count)
